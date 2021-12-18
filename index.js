628ce1ff5df86f32e5ce511afe396cacd4aa0eb9
darling_d <ddarling212@gmail.com>
2021-12-17 21:12:33 -0500

    Added bell + tree animations plus new photo background

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const pauseBtn = document.getElementById("pause-btn")
 const stopBtn = document.getElementById("stop-btn")
 let audio = new Audio('bells.mp3');
 let volume = document.getElementById("volume-control");
+const bell = document.getElementById("bell");
+const tree = document.getElementById("tree");
 
 // Task:
 // - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
@@ -10,17 +12,26 @@ playBtn.addEventListener("click", play);
 pauseBtn.addEventListener("click", pause);
 stopBtn.addEventListener("click", stop);
 
+// New Task:
+// - Animate the bell so that it looks like it is ringing when the music is playing, and stops when the music is paused or stopped.
+
 function play() {
     audio.play();
+    bell.classList.add("animate-bell");
+    tree.classList.add("animate-tree");
 }
 
 function pause() {
     audio.pause();
+    bell.classList.remove("animate-bell");
+    tree.classList.remove("animate-tree");
 }
 
 function stop() {
     audio.pause();
     audio.currentTime = 0;
+    bell.classList.remove("animate-bell");
+    tree.classList.remove("animate-tree");
 }
 
 // Stretch goals:
@@ -29,5 +40,4 @@ function stop() {
 volume.addEventListener("change", function(e) {
     audio.volume = e.currentTarget.value / 100;
 });
-
 
